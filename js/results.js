6a8ee0bac304d5eeb38f15faddc5425b261a1705
Boris Xiao <borishaw@gmail.com>
2015-09-10 15:26:50 -0400

    copy to clipboard fix

--- js/results.js
+++ js/results.js
@@ -1,62 +1,61 @@
 // Email Footer Generator Scripts
 
 
- 
- $(function() {
-	if ($('#results-tabs').length) {
-		$('#results-tabs').tabtab({
-			tabMenu: '.tab-menu',             // direct container of the tab menu items
-			tabContent: '.tab-content',       // direct container of the tab content items
-			startSlide: 1,                    // starting slide on pageload
-			// dynamicHeight: true,                // if true the height will dynamic and animated.
-			arrows: false,                       // keyboard arrow navigation
-			animateHeight:!1,fixedHeight:!1
-		});
-	}
-	
-	$('.open-popup-link').magnificPopup({
-		type:'inline',
-		midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-		fixedContentPos: false,
-		fixedBgPos: true,
-		overflowY: 'auto',
-		closeBtnInside: true,
-		preloader: false,
-		removalDelay: 300,
-		mainClass: 'my-mfp-zoom-in',
-		gallery:{
-			enabled:true
-		}
-	 });
-	
-	// Zero Clipboard
-	var client = new ZeroClipboard( document.getElementById("copy-button") );
+$(function () {
+    if ($('#results-tabs').length) {
+        $('#results-tabs').tabtab({
+            tabMenu: '.tab-menu',             // direct container of the tab menu items
+            tabContent: '.tab-content',       // direct container of the tab content items
+            startSlide: 1,                    // starting slide on pageload
+            // dynamicHeight: true,                // if true the height will dynamic and animated.
+            arrows: false,                       // keyboard arrow navigation
+            animateHeight: !1, fixedHeight: !1
+        });
+    }
 
-    client.on( "ready", function( readyEvent ) {
-         //alert( "ZeroClipboard SWF is ready!" );
+    $('.open-popup-link').magnificPopup({
+        type: 'inline',
+        midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+        fixedContentPos: false,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in',
+        gallery: {
+            enabled: true
+        }
+    });
 
-        client.on( "aftercopy", function( event ) {
-            //event.target.style.display = "none";
-            //alert("Copied text to clipboard: " + event.data["text/plain"] );
-				// $('#copy-button').html("Copied!");
-				$('#copy-button-prompt').fadeIn().delay(1000).fadeOut();
-				// document.selection.empty();
-				window.getSelection().removeAllRanges();
-		  } );
-    } );
-     
+
+
+
 });
-
-
-
 
 
 // COPY CODE FUNCTION
 function copyCode() {
-		$('#footer-download').val(($('#results-visual').html()));
-  }
+    $('#footer-download').val(($('#results-visual').html()));
+}
 
-//Grab the footer code and append it to the XMP tag
-$(document).ready(function(){
-	$('#code-snippet').text($('#results-visual').html());
+$(document).ready(function () {
+    //Grab the footer code and append it to the XMP tag
+    $('#code-snippet').text($('#results-visual').html());
+
+    var client = new ZeroClipboard(document.getElementById("copy-button"));
+
+    // Zero Clipboard
+    client.on("ready", function (readyEvent) {
+        console.log( "ZeroClipboard SWF is ready!" );
+
+        client.on("aftercopy", function (event) {
+            //event.target.style.display = "none";
+            //alert("Copied text to clipboard: " + event.data["text/plain"] );
+            // $('#copy-button').html("Copied!");
+            $('#copy-button-prompt').fadeIn().delay(1000).fadeOut();
+            // document.selection.empty();
+            window.getSelection().removeAllRanges();
+        });
+    });
 });
